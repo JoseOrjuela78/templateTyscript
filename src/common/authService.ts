26aa39f62681ace225f2314a2f23f34d0622c977
Jose Alfredo Orjuela Aldana <D1201755@CorredorEmpresarial.com.co>
2026-07-15 16:56:07 -0500

    middleware privilegios

--- src/common/authService.ts
+++ src/common/authService.ts
@@ -3,13 +3,19 @@ import  jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import e from './config/enviroment-vars';
 import ResponseManagenent from './responseManagement';
+import msDatabase from './database/data-mssql';
+import sql from 'mssql';
+import { UtilService } from './utilsService';
 
 class AuthService{
     private resManagement:ResponseManagenent;
     private jwtKey:string;
+    private db: typeof msDatabase
+
     constructor(){
         this.resManagement = new ResponseManagenent();
         this.jwtKey = e.envs.JWT_KEY;
+        this.db = msDatabase;
     }
 
    async comparePassword(bodyPass:string, dbPass:string) {
@@ -28,14 +34,34 @@ class AuthService{
         return token;
    };
 
+   private async verificaPermits(req:any, res:Response){
+     try {
+        const result:any = this.db.executeStoredProcedure( 
+            'PR_GET_PATH_PERMITIDO',
+            {
+                ID_ROL: { type: sql.Int, value: req.user.ID_ROL}
+            },
+            {
+                STATUS_CODE: sql.Int,
+                STATUS_DESC: sql.VarChar(500)
+            }
+        );
+       
+
+    const dbPaths = result.recordsets[0];
+    return dbPaths;
+
+    } catch (error:any) {
+        return this.resManagement.responseError(req,res,error.code,error.message);
+    };
+
+   };
+
     verificaToken(req:any, res:Response, next:NextFunction){
 
             const authHeader = req.headers['authorization'];
             // Verificamos que exista y tenga el formato correcto
-            if (!authHeader || !authHeader.startsWith('Bearer ')) {
-               return this.resManagement.responseError(req,res,422,'Token not provided or invalid');
-            };
-
+            if (!authHeader || !authHeader.startsWith('Bearer '))return this.resManagement.responseError(req,res,422,'Token not provided or invalid');
             // Extraemos el token
             const token = authHeader.split(' ')[1];
 
@@ -43,9 +69,24 @@ class AuthService{
                  
                 if (err) return this.resManagement.responseError(req,res,401,err);
                 req.user = decoded;
+
+                const dbPaths = await this.verificaPermits(req,res);
+                if(dbPaths.length <= 0)return this.resManagement.responseError(req,res,401,'Insufficient privileges');
+                
+                let sts = false;
+                for (let per of dbPaths) {
+                    if (req.method === per.method && UtilService.pathToRegex(per.path, req.originalUrl)) {
+                     sts = true;
+                     break;
+                   };
+                };
+
+                if(!sts)return this.resManagement.responseError(req,res,401,'Insufficient privileges');
+         
                 next();
+         
             });
-    }
+    };
 
 }
 
